9bd8825efade683a5fa6c5397603a27497be565f
Abhijith V Mohan <abhijith.vmohan@gmail.com>
2019-11-27 22:26:59 +0530

    Fix jira

--- lib/jira.zsh
+++ lib/jira.zsh
@@ -1,24 +1,38 @@
+# Dependencies - go-jira, fzf, hub
+
 if [[ -z $BRANCH_PREFIX ]]; then
   BRANCH_PREFIX=$(whoami)
 fi
 
 if [[ -z $JQL ]]; then
-  #JQL='assignee = currentUser() AND sprint in openSprints() AND statusCategory != done ORDER BY updated DESC'
-  JQL='assignee = currentUser() AND statusCategory != done ORDER BY updated DESC'
+  JQL='assignee = currentUser() AND sprint in openSprints() AND statusCategory != done ORDER BY updated DESC'
+  #JQL='assignee = currentUser() AND statusCategory != done ORDER BY updated DESC'
 fi
+
+jls() {
+  jira list --query "$JQL" | fzf
+}
 
 jdesc() {
   jira view $1 --gjq 'fields.summary'
 }
 
 jid() {
-  jira list --query "$JQL" | fzf | sed -r 's/^([A-Z]+-[0-9]+):.*/\1/'
+  jls | sed -r 's/^([A-Z]+-[0-9]+):.*/\1/'
+}
+
+jmsg() {
+  jls | sed -r 's/^([A-Z]+-[0-9]+):\s+(.*)/[\1]: \2/'
+}
+
+jbr() {
+  echo $BRANCH_PREFIX/$(jls | sed -r -e 's/^([A-Z]+-[0-9]+):\s+(.*)/\1-\L\2/' -e 's/\s+/-/g' -e 's/-$//' -e 's/[^A-Za-z0-9-]+//g' -e 's/-+/-/g')
 }
 
 mkbr() {
   src=${1:-origin/master}
-  jid=$(jid)
-  git checkout -b $BRANCH_PREFIX/$jid-$(jdesc "$jid" | sed -r 's/\s+/-/g' | tr 'A-Z' 'a-z' | sed -r 's/[^a-z0-9-]+//g') $src
+  br_name=$(jbr)
+  git checkout -b $br_name $src
 }
 
 # Make a PR from current branch to master
